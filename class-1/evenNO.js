9010b3674d0340b05fcfd324da791d2f53a8c1a6
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-01-25 20:29:16 +0530

    .

---
+++ class-1/evenNO.js
@@ -0,0 +1,11 @@
+
+
+let arr = [2,323,434,521,5356,432,5467,321,3434,5675,6657]
+
+let length = arr.length;
+
+for (let i = 0 ; i<=length-1 ;i++){
+    if(arr[i]%2==0){
+        console.log(arr[i]);
+    }
+}
