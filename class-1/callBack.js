6f9d085d1db34920e301cfbf5713f6b0b483c97c
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-01-28 23:27:12 +0530

    .

--- class-1/callBack.js
+++ class-1/callBack.js
@@ -2,7 +2,7 @@
 
 function sum(a,b, callback){
     let added = a+b;
-
+    callback(added);
 }
 
 function displayActive(displayingText){
@@ -13,6 +13,5 @@ function displayPassive(displayingText){
     console.log(`the sum is in passive form ${displayingText}`);
 }
 
-let a = 10;
-let b = 5;
-let wantedResult = sum(a,b,displayActive);
+
+let wantedResult = sum(5,10,displayPassive);
