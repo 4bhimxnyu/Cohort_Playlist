f41f1a722a6dbd9172211806b0ffd6a9d1ea98f6
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-01-30 00:08:45 +0530

    .

---
+++ class-1/string/length.js
@@ -0,0 +1,9 @@
+// calculates the length of the function 
+
+function getLength(str){
+    console.log("original string is: ",str);
+    let len = str.length;
+    console.log(`length of the string is: ${len}`);
+}
+
+getLength("Welcome to the world of JavaScript");
