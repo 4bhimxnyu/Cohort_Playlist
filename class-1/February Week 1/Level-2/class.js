ca7451b63a6366a7231c1a9afba8af30e72afa7c
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-02-03 21:07:27 +0530

    .

--- class-1/February Week 1/Level-2/class.js
+++ class-1/February Week 1/Level-2/class.js
@@ -1,21 +1,22 @@
-// this is a demonstration of class from oop logic
+// This is a demonstration of a class from OOP logic
 
-class Students{
-    constructor(Name,section,rollNo,EnrollmentId){
-        this.Name = Name;
+class Students {
+    constructor(firstName, section, rollNo, enrollmentId) {
+        this.firstName = firstName;  // Fixed typo (was firsName)
         this.section = section;
         this.rollNo = rollNo;
-        this.EnrollmentId= EnrollmentId;
+        this.enrollmentId = enrollmentId;
     }
-    display(){
-        console.log(`the name of the student is ${Name} and the roll no is ${rollNo} and the enrollment id is ${EnrollmentId}`);
-        
+
+    display() {
+        console.log(`The name of the student is ${this.firstName}, the roll number is ${this.rollNo}, and the enrollment ID is ${this.enrollmentId}.`);
     }
 }
 
-let Student1 = new Students("Abhimanyu","B","02","5632AP");
+// Creating instances
+let Student1 = new Students("Abhimanyu", "B", "02", "5632AP");
+let Student2 = new Students("Aishwarya", "C", "05", "1242AP");
 
-let Student2 = new Students("Aishwarya","C","05","1242AP");
-
+// Displaying information
 Student1.display();
 Student2.display();
