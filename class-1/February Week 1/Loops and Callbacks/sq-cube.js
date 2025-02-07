f3ddc6cd576a219d0cffd45edd47903652b9d103
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-02-07 23:50:04 +0530

    .

---
+++ class-1/February Week 1/Loops and Callbacks/sq-cube.js
@@ -0,0 +1,16 @@
+function sqaure(n){
+    return n*n;
+}
+
+function cube(n){
+    return n*n*n;
+}
+
+function addSumCube(value1 , value2 , cb){
+    let val1 = cb(value1)
+    let val2 = cb(value2)
+    return val1+ val2;
+}
+
+let answer = addSumCube(2,3,sqaure)
+console.log(answer);
