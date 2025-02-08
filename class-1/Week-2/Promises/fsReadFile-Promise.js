af3a29cb5e9771298d161d99e1463da0acf31bda
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-02-08 23:41:04 +0530

    fs.readFile error handling

--- class-1/Week-2/Promises/fsReadFile-Promise.js
+++ class-1/Week-2/Promises/fsReadFile-Promise.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
 
-function calling(){
-    return new Promise(function(resolve){
-      fs.readFile('text.txt', 'utf-16le' , function(err,data){
-        resolve(data)
-      })  
-    })
+function calling() {
+    return new Promise(function (resolve, reject) {
+        fs.readFile('text.txt', 'utf-8', function (err, data) {
+            if (err) {
+                reject(err); // Rejecting the Promise if an error occurs
+            } else {
+                resolve(data);
+            }
+        });
+    });
 }
 
-function onDone(data){
-    console.log(data)
+function onDone(data) {
+    console.log(data);
 }
 
-calling().then(onDone);
+function onError(error) {
+    console.error("Error reading file:", error);
+}
+
+// Handling both success and error cases
+calling().then(onDone).catch(onError);
