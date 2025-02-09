a10489fa500d254f96bcb89783ba453787dd164e
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-02-09 23:21:35 +0530

    rejected promise program //  needs to be fixed

--- class-1/Week-2/Promises/success.js
+++ class-1/Week-2/Promises/success.js
@@ -1,4 +1,25 @@
+// this has got bugs and errors which needs to be resolved
+
 let cohort = true ;
 let learning = false
 
-function
+function learningCurve(){
+     return new Promise(function(resolve, reject){
+        if(cohort){
+            resolve("Successful cohort");
+        }
+        else if(learning){
+            resolve("Successful in learning")
+        }
+        else{
+            reject("nothing was a success")
+        }
+    })
+   
+}
+
+learningCurve.then(function(message){
+    console.log("the promise was resolved" + message );
+}).catch(function(message){
+    console.log(`the promise was rejected ${message}`);
+})
