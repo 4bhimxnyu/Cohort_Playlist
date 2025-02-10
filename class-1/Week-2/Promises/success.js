79fc9b1db1a657ad4cbf5b559a718fa7f98c2de4
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-02-10 18:55:14 +0530

    Promises problem resolved with modification

--- class-1/Week-2/Promises/success.js
+++ class-1/Week-2/Promises/success.js
@@ -6,20 +6,29 @@ let learning = false
 function learningCurve(){
      return new Promise(function(resolve, reject){
         if(cohort){
-            resolve("Successful cohort");
+            resolve({
+                name : "cohort" , 
+                message : "this is a banger"
+            });
         }
         else if(learning){
-            resolve("Successful in learning")
+            resolve({
+                name : "kearning" , 
+                message : "learning is never stopping"
+            })
         }
         else{
-            reject("nothing was a success")
+            reject({
+                name : "error" ,
+                message : "error in learning curve"
+            })
         }
     })
    
 }
 
-learningCurve().then(function(message){
-    console.log("the promise was resolved" + message );
+learningCurve().then(function(msg){
+    console.log("the promise was resolved " + msg.message );
 }).catch(function(message){
-    console.log(`the promise was rejected ${message}`);
+    console.log(`the promise was rejected ${msg}`);
 })
