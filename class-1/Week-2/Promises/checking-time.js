2336d8b22ebc0c828b3533dd591ed8762cfe5c1e
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-02-22 19:19:58 +0530

    code analytics

--- class-1/Week-2/Promises/checking-time.js
+++ class-1/Week-2/Promises/checking-time.js
@@ -3,13 +3,17 @@ console.log("this should be first")
 function promisifiedFunction(){
     return new Promise(function(resolve){
         setTimeout(function (){
-            resolve("this is the last");
-        },2000);
+            console.log("this is the third");
+            resolve("this is the sixth");
+            console.log("this is the fourth");
+        },500);
     })
 }
 
 console.log("this is second");
 
 promisifiedFunction().then(function(value){
+    console.log("this is the fifth")
     console.log(value)
+    console.log("this is the last")
 })
