0a2c8a1c93fc5777c145014c06f7c1cce411f113
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-02-22 16:05:40 +0530

    settimeout function through promises

--- class-1/Week-2/Promises/settimeout.js
+++ class-1/Week-2/Promises/settimeout.js
@@ -1,6 +1,6 @@
 // asynchronous set timeout function using the logics of promises
 
-const { log } = require("three/tsl");
+
 
 function setTimoutActivate(duration){
     let p = new Promise(function(resolve){
@@ -8,7 +8,7 @@ function setTimoutActivate(duration){
     })
     return p;
 }
-
+ 
 const ans = setTimoutActivate(1000)
 ans.then(function(){
     console.log("the timeout has been activated");
