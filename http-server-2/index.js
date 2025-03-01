9d05300c7d9548a0de80c98b94188ec9dee50a28
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-03-01 20:06:41 +0530

    added more res hahaha

--- http-server-2/index.js
+++ http-server-2/index.js
@@ -11,6 +11,10 @@ app.get("/route-handler" , function(req,res){
     })
 })
 
+app.get("/" , function(req,res){
+    res.send("Hello World")
+})
+
 app.listen(port, function(){
     console.log(`Example app listening on port ${port}`)
   }) 
