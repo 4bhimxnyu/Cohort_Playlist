fed4f5494a0d413e856e6a935027e7617004de1a
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-03-01 20:46:03 +0530

    json stringyfy to res

--- http-server-2/index.js
+++ http-server-2/index.js
@@ -15,6 +15,15 @@ app.get("/" , function(req,res){
     res.send("Hello World")
 })
 
+app.get("/about" , function(req,res){
+    res.json({
+        "Page" : "this is the about page for the user" , 
+        "firstName" : "Abhimanyu" ,
+        "lastName" : "Singh" ,
+        "age" : 18
+    })
+})
+
 app.listen(port, function(){
     console.log(`Example app listening on port ${port}`)
   }) 
