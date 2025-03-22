c1164208935f58accdef6345eef1a882497c694a
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-03-22 18:40:42 +0530

    arrow functions demonstration

---
+++ class-1/Some Js functions/arrow.js
@@ -0,0 +1,17 @@
+// this is how the normal function works
+function product(a,b){
+    return a*b;
+}
+
+let amt_prod = product(3,4);
+console.log(amt_prod)
+
+
+// this is how arrow function works
+
+const sum = (a,b) =>{
+    return a+b;
+}
+
+const ans = sum(3,4);
+console.log(ans);
