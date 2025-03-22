7c799876880b887844ffe7425d173a98974ac11c
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-03-22 18:10:34 +0530

    file reading system backend

--- File server/file.js
+++ File server/file.js
@@ -1,10 +1,17 @@
 const express = require('express');
-
+const fs = require('fs')
 const app = express()
 
 
 app.get("/files/:fileName", function(req,res){
     const name = req.params.fileName;
+
+    fs.readFile(name , "utf-8", function(err,data){
+        res.json({
+            data
+        })
+    });
+
     console.log(name);
     res.json({})
 })
