08216b41582f4ca8d2d97f081f1f2afce093ebe3
Abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-06-29 03:13:52 +0530

    ip-server, people fetch server update

--- Express.js codes/fetch/ip address api/index.js
+++ Express.js codes/fetch/ip address api/index.js
@@ -3,5 +3,6 @@ async function validateIP() {
     const data = await response.json();
     const result = await fetch(`https://api.ipinfo.io/lite/${data.ip}?token=b75f4b11a81ed4`);
     const ipData = await result.json();
+    document.getElementById("ip").innerHTML = `IP Address: ${ipData.ip} <br> asn: ${ipData.asn} <br> Continent: ${ipData.continent} <br> Country: ${ipData.country}`;
     console.log(ipData);
 }
--- Express.js codes/fetch/people api/index.js
+++ Express.js codes/fetch/people api/index.js
@@ -1,5 +1,12 @@
 async function validatePeople(){
     const response = await fetch("https://fakerapi.it/api/v1/persons");
     const data = await response.json();
+    document.getElementById("people").innerHTML = `
+        <h2>People Data</h2>
+        <ul>
+            ${data.data.map(person => `<li>${person.firstname} ${person.lastname} - Email: ${person.email}</li>`).join('')}
+        </ul>
+    `;
+    // Display the data in the console for debugging
     console.log(data);
 }
