fe4a268479952aa2c6c41bd014481b5379e1c1de
Abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-08-18 10:04:49 +0530

    jwt authentication

--- Express.js codes/JWT/authJWT.js
+++ Express.js codes/JWT/authJWT.js
@@ -53,7 +53,11 @@ app.post('/login',(req,res)=>{
             msg :"user is not found in the database"
         })
     }
-
+    // if the user exists, we will create a token for the user
+    // we will use the jwt.sign method to create a token for the user
+    // the first argument is the payload, which is an object that contains the data we want
+    // to store in the token, the second argument is the secret key that we will use
+    // to sign the token, and the third argument is an options object that contains the` 
     var token = jwt.sign({ username },jwtSecret);
     res.json({
         
