586f50298f40b05c3e532a912b780ba0b08b1564
4bhimxnyu <abhimanyupratapsingh30nov1106@gmail.com>
2026-02-02 11:12:45 +0530

    manually wrote foreach function

--- class-1/Week 1/Level-1/Array/foreach.js
+++ class-1/Week 1/Level-1/Array/foreach.js
@@ -4,4 +4,12 @@ function logThing(item){
     console.log(item)
 }
 
+function logagain(arr){
+    for(let i = 0 ; i <= arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+
+let result = logagain(array1);
+console.log(result);
 array1.forEach(logThing);
